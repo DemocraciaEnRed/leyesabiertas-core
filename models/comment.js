d8a54b180c36f96eec4341fb66e4010c73279f0d
Guillermo Croppi <guillermocroppi@gmail.com>
2019-07-10 21:40:05 -0300

    DERLA-65 Fix error stack=TypeError: timeago is not a function & cannot mongoose.connect() multiple times while connected

--- models/comment.js
+++ models/comment.js
@@ -16,7 +16,7 @@ const Comment = new mongoose.Schema({
 
 Comment.post('find', function (doc) {
   doc = doc.map((d) => {
-    d.when = timeago().format(d.createdAt, 'es_AR')
+    d.when = timeago.format(d.createdAt, 'es_AR')
     if (d.decoration) {
       d.decoration.mark.data = {}
       d.decoration.mark.data.id = d._id
